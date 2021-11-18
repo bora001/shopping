702e15649e2bbd81681b1359b4ce76ccee9d1ef8
bora001 <bora.vancouver@gmail.com>
2021-11-18 14:00:32 -0800

    mongDB connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
-
 const port = process.env.PORT || 5000;
+const config = require("./config/key");
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is connected..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("This is Shopping Page"));
 app.listen(port, () => console.log(`localhost:${port}`));
